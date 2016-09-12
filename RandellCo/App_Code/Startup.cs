e0ed81f7d24078932abf95bbfd95a98dd4e184ba
rco1 <randell_co1996@yahoo.com>
2016-09-12 16:18:56 -0600

    creating new website
    all the dll on creation

---
+++ RandellCo/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RandellCo.Startup))]
+namespace RandellCo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
